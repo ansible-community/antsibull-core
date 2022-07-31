b6be593938d5a67cb43699177d81ecfafdd0e9d2
Felix Fontein <felix@fontein.de>
2022-07-31 18:37:40 +0200

    Fix license string for src/antsibull_core/vendored/json_utils.py.

--- src/antsibull_core/vendored/json_utils.py
+++ src/antsibull_core/vendored/json_utils.py
@@ -1,5 +1,4 @@
-# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
-# https://www.gnu.org/licenses/gpl-3.0.txt)
+# BSD 2-clause license (see LICENSES/BSD-2-Clause.txt)
 # SPDX-FileCopyrightText: Ansible Project
 # SPDX-License-Identifier: BSD-2-Clause
 
