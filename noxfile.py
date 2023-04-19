4d15ea4681877d96e542cb7bcf2e2530eab0d8e6
Felix Fontein <felix@fontein.de>
2023-04-19 07:46:36 +0200

    Fix release step in noxfile: convert Path to str.

--- noxfile.py
+++ noxfile.py
@@ -178,7 +178,7 @@ def bump(session: nox.Session):
         )
         with open(fragment_file, "w") as fp:
             print(fragment, file=fp)
-        session.run("git", "add", "pyproject.toml", fragment_file, external=True)
+        session.run("git", "add", "pyproject.toml", str(fragment_file), external=True)
         session.run("git", "commit", "-m", f"Prepare {version}.", external=True)
     session.run("antsibull-core", "release")
     session.run(
