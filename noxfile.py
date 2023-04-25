2d1281b9ac53090fde15fe330e0a84d86fcfdfc0
Felix Fontein <felix@fontein.de>
2023-04-25 12:43:06 +0200

    CI: remove in valid GHA parameters, make pyre work in CI (#73)
    * pyre does not work when we don't install ourself in editable mode.
    
    * Use correct options for codecov/codecov-action.

--- noxfile.py
+++ noxfile.py
@@ -93,7 +93,8 @@ def codeqa(session: nox.Session):
 
 @nox.session
 def typing(session: nox.Session):
-    install(session, ".[typing]", editable=True)
+    # pyre does not work when we don't install ourself in editable mode 🙄.
+    install(session, "-e", ".[typing]")
     session.run("mypy", "src/antsibull_core")
 
     purelib = session.run(
