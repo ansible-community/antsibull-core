c0d33b0f008d0c8f9a7bf1fa82c6446178005eca
Maxwell G <maxwell@gtmx.me>
2023-11-28 23:57:01 +0000

    store package version in __version__ constant
    This allows using the `hatch version` tool to automatically update
    versions and makes sure the version metadata of editable installs is
    always up to date.

--- src/antsibull_core/__init__.py
+++ src/antsibull_core/__init__.py
@@ -2,3 +2,13 @@
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020, Ansible Project
+
+"""
+Shared code used by tools for building the Ansible distribution
+"""
+
+from __future__ import annotations
+
+__version__ = "2.1.0.post0"
+
+__all__ = ("__version__",)
