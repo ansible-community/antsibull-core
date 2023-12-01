451849dfde2b40c17fb0e84c61f22b8ed885657e
Maxwell G <maxwell@gtmx.me>
2023-12-01 20:31:44 +0000

    Prepare 2.2.0.

--- src/antsibull_core/__init__.py
+++ src/antsibull_core/__init__.py
@@ -9,6 +9,6 @@ Shared code used by tools for building the Ansible distribution
 
 from __future__ import annotations
 
-__version__ = "2.1.0.post0"
+__version__ = "2.2.0"
 
 __all__ = ("__version__",)
