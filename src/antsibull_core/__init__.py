a05a7928bcaa2fc79eee10614d9243f3d06f2d9b
Maxwell G <maxwell@gtmx.me>
2024-02-15 02:11:34 +0000

    Prepare 3.0.0a1.

--- src/antsibull_core/__init__.py
+++ src/antsibull_core/__init__.py
@@ -9,6 +9,6 @@ Shared code used by tools for building the Ansible distribution
 
 from __future__ import annotations
 
-__version__ = "3.0.0.dev0"
+__version__ = "3.0.0a1"
 
 __all__ = ("__version__",)
